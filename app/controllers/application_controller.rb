3b46a8ca612f8b190debee1bcdc158732aeaaedb
Hugo <huwgoma@gmail.com>
2022-08-23 17:37:51 -0400

    Disable Turbo Drive on registrations/new form; permit name parameter for Devise's sign_up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  # Add name param to User
+  before_action :configure_permitted_params, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 end
