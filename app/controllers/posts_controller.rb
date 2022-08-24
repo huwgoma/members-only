009787f056fa3e685417b462e3b066cf40b42aa8
Hugo <huwgoma@gmail.com>
2022-08-24 01:42:51 -0400

    Write PostsController#create and #show; after creating a new post, redirect to its show view - in the show view, display the post's title and body, and if the user is logged in, the post's author

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,6 +7,22 @@ class PostsController < ApplicationController
   end
 
   def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      redirect_to @post
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
+  def show 
+    @post = Post.find(params[:id])
+  end
+
+  private 
+
+  def post_params
+    params.require(:post).permit(:title, :body, :user_id)
+  end
 end
