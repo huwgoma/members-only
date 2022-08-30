596a9153b02c85613c76b4a99f97940f67a2da96
Hugo <huwgoma@gmail.com>
2022-08-30 12:20:31 -0400

    Write PostsController#update

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -31,6 +31,16 @@ class PostsController < ApplicationController
     redirect_to post_path
   end
 
+  def update
+    @post = Post.find(params[:id])
+
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   private 
 
   def post_params
